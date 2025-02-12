f83037199eaa4478a932a3bc5ed1bab5b0849943
roknuzzaman <rokon.raz@gmail.com>
2025-02-12 18:48:54 +0600

    Add credentials login using load_dotenv and import some libarys.

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import os
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import pandas as pd
+
+app = Flask(__name__)
+load_dotenv()
+
+# Database connection details
+db_host = os.getenv('DB_HOST')
+db_user = os.getenv('DB_USER')
+db_pass = os.getenv('DB_PASSWORD')
+db_name = os.getenv('DB_NAME')
+
+DATABASE_URL = f"mysql+pymysql://{db_user}:{db_pass}@{db_host}/{db_name}"
+engine = create_engine(DATABASE_URL)
+
+
